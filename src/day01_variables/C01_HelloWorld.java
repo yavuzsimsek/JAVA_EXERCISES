9bd1c34e7df4684fc540bf9a293b6c569c0be2fa
yavuz <benyavuzsimsek@gmail.com>
2023-07-03 21:15:16 +0300

    day02

--- src/day01_variables/C01_HelloWorld.java
+++ src/day01_variables/C01_HelloWorld.java
@@ -1,7 +1,9 @@
 package day01_variables;
 
-public class C01_Variables {
+public class C01_HelloWorld {
     public static void main(String[] args) {
+
+        // this i about how to write on console
         System.out.println("Hello Java, Hello World");
     }
 }
---
+++ src/day02_variables/C01_Variables_HelloWorld.java
@@ -0,0 +1,15 @@
+package day02_variables;
+
+public class C01_Variables_HelloWorld {
+    public static void main(String[] args) {
+
+        int not = 60; //declaration variable and assign a value.
+        int not2 = 70;
+
+        int ort = (not + not2) /2;
+
+        System.out.println("Result = " + ort);
+
+
+    }
+}
